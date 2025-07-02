78bc7e5919ffb89d2bb251fc8ef2e7155395b203
mecaneer23 <mecaneer23@gmail.com>
2025-07-02 12:41:51 -0500

    feat: begin to implement header and footer fetching over the network

--- customizations.js
+++ customizations.js
@@ -7,8 +7,31 @@ function setCustomTitlePrefix(titlePrefix) {
     }
 }
 
-function setCustomizations() {
+async function fetchRemoteHTML(url) {
+    return await fetch(url)
+        .catch(error => {
+            console.error('Error fetching remote HTML:', error);
+        })
+        .then(response => {
+            if (!response.ok) {
+                console.error(`HTTP error! status: ${response.stack}`);
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.text();
+        });
+}
+
+async function setHeaderFooter() {
+    const style = document.createElement("link");
+    style.href = "https://raw.githubusercontent.com/mecaneer23/mecaneer23.github.io/refs/heads/main/resources/nav.html";
+    // style.re
+    document.querySelector('header').innerHTML = await fetchRemoteHTML("https://raw.githubusercontent.com/mecaneer23/mecaneer23.github.io/refs/heads/main/resources/nav.html");
+    document.querySelector('footer').innerHTML = await fetchRemoteHTML("https://raw.githubusercontent.com/mecaneer23/mecaneer23.github.io/refs/heads/main/resources/footer.html");
+}
+
+async function setCustomizations() {
     setCustomTitlePrefix(TITLE_PREFIX);
+    // await setHeaderFooter();
 }
 
 setCustomizations();
